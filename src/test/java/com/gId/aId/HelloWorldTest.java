d7fc20e511cf0c160e092ec63222f96fec378011
wenliyi <1500022715@pku.edu.cn>
2016-06-25 10:26:55 +0800

    提交信息，一定要写

---
+++ src/test/java/com/gId/aId/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.gId.aId;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloWorldTest
+{
+	@Test
+	public void testSayHello()
+	{
+		HelloWorld helloWorld = new HelloWorld();
+
+		String result = helloWorld.sayHello();
+
+		assertEquals("Hello Maven",result);
+	}
+}
